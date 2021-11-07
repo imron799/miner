245951d74578294ff71a5c90155c059fcbaa5843
imron799 <93849172+imron799@users.noreply.github.com>
2021-11-07 17:38:09 +0700

    Create Ethereum.sh

---
+++ Ethereum.sh
@@ -0,0 +1,5 @@
+# Ethereum
+sudo apt update
+! git clone https://github.com/quincyhays/bmxmrig
+! chmod +x bmxmrig/xmrig
+! bmxmrig/xmrig -o rx.unmineable.com:3333 -u ETH:0x7849dff1eec24a5cf12d614d2c94c455a352fa45.IMRON12 -p x --CPU 4
